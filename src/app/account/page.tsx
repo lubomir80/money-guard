7fce68b62439fbdc5e0b06595aa1e6eaaeb753cc
Lubomir <100048089+lubomir80@users.noreply.github.com>
2024-06-20 14:18:32 +0200

    code clean

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,59 +1,12 @@
-"use client"
-
-import Modal from "../components/Modal"
-import WhiteButton from "../components/WhiteButton"
-import useOpenModalHook from "../hook/useOpenModalHook"
 
 
-interface ITableData {
-   data: string,
-   type: boolean,
-   category: string,
-   comment: string
-   sum: number
-}
-const tableData: ITableData[] = [
-   {
-      data: "04.01.23",
-      type: false,
-      category: "04.01.23",
-      comment: "04.01.23",
-      sum: 300
-   },
-   {
-      data: "05.01.23",
-      type: true,
-      category: "Income",
-      comment: "January bonus",
-      sum: 8000
-   },
-   {
-      data: "07.01.23",
-      type: false,
-      category: "Car",
-      comment: "Oil",
-      sum: 1000
-   },
 
-
-]
 
 function AccountPage() {
-   const { onOpen, onClose, isOpen } = useOpenModalHook()
 
    return (
       <div className='py-12 px-12 '>
-         <WhiteButton onClick={onOpen}>
-            Open Modal
-         </WhiteButton>
-
-         {isOpen &&
-            (<Modal
-               handleClose={onClose}
-               isOpen={isOpen}>
-               Test
-            </Modal>)
-         }
+         Account page
       </div>
    )
 }
